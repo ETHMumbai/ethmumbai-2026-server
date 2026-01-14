4087c74aba3f84855d93b9f33a4526ddb0d5809e
Diksha <dikshas2911@gmail.com>
2026-01-14 20:08:15 +0530

    chore:rzp payment method

--- src/tickets/generateInvoice.ts
+++ src/tickets/generateInvoice.ts
@@ -5,15 +5,48 @@ import {
   InvoiceData,
 } from '../utils/generateInvoicePdf';
 import { generateInvoiceNumberForOrder } from 'src/utils/ticket.utils';
+import Razorpay from 'razorpay';
 @Injectable()
 export class InvoiceService {
-  constructor(private prisma: PrismaService) {}
+  private razorpay: Razorpay;
+  constructor(private prisma: PrismaService) {
+    this.razorpay = new Razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET,
+    });
+  }
   private async buildInvoiceData(
     order: any, // Prisma order with includes
   ): Promise<InvoiceData> {
     const buyer = order.buyer;
     const address = buyer.address;
     const ticket = order.ticket;
+
+    let rzpLabel = '';
+
+    if (order.paymentType == 'RAZORPAY' && order.razorpayPaymentId != null) {
+      const payment = await this.razorpay.payments.fetch(
+        order.razorpayPaymentId,
+      );
+
+      // Convert to nice label for UI
+      switch (payment.method) {
+        case 'upi':
+          rzpLabel = 'UPI via Razorpay';
+          break;
+        case 'card':
+          rzpLabel = 'Card via Razorpay';
+          break;
+        case 'netbanking':
+          rzpLabel = 'Netbanking via Razorpay';
+          break;
+        case 'wallet':
+          rzpLabel = 'Razorpay';
+          break;
+        default:
+          rzpLabel = payment.method;
+      }
+    }
 
     return {
       invoiceNo: order.invoiceNumber,
@@ -38,7 +71,7 @@ export class InvoiceService {
 
       paymentMethod:
         order.paymentType === 'RAZORPAY'
-          ? 'INR (Razorpay)'
+          ? `INR (${rzpLabel || 'Unknown'})`
           : 'Crypto',
     };
   }
@@ -60,10 +93,7 @@ export class InvoiceService {
       return generateInvoicePDFBuffer(invoiceData);
     }
 
-    const invoiceNo = await generateInvoiceNumberForOrder(
-      this.prisma,
-      orderId,
-    );
+    const invoiceNo = await generateInvoiceNumberForOrder(this.prisma, orderId);
 
     const invoiceData = await this.buildInvoiceData({
       ...order,
